b647a6f050009b750a2d396f789adf0771f18179
Ivan Cuadrado <cuadradoivan1990@gmail.com>
2022-07-28 00:41:32 -0300

    Publish github

--- src/Productos/controller.js
+++ src/Productos/controller.js
@@ -1,15 +1,21 @@
 const debug = require('debug')('app:products-controller');
+const createError = require('http-errors')
 
 const {ProductsService} = require('./services')
+const {Response} = require('../common/response')
 
 module.exports.ProductsController = {
     getProduct: async (req, res) => {
         try {
             let product = await ProductsService.getById(req.params.id);
-            res.json( product)
+            if (!product)
+                Response.error(res, new createError.NotFound)
+            Response.success(res, 200, 'Producto ' + req.params.id, product)
+            // res.json( product)
         } catch (error) {
             debug('Algo anda mal', error);
-            res.status(500).json({error: error})
+            // res.status(500).json({error: error})
+            Response.error()
         }
         // res.send('getAll');
     },
@@ -17,22 +23,25 @@ module.exports.ProductsController = {
         try {
             let products = await ProductsService.getAll();
             // return products
+            Response.success(res, 200, 'Lista de productos', products)
             res.status(200).json({products})
         } catch (error) {
             debug('Algo anda mal', error);
-            res.status(500).json({error: error})
+            Response.error()
         }
        // res.send('getProduct')
     }, 
     createProduct: async (req, res) => {
         try {
-            const {body} = req
             debug('Creating product', req.headers);
+            const {body} = req
+            if (!body || Object.keys(body).length === 0) Response.error(res, new createError.BadRequest())  
+            
             let product = await ProductsService.create(req.body);
             res.status(200).json({product})
         } catch (error) {
             debug('Algo anda mal', error);
-            res.status(500).json({error: error})
+            Response.error()
         }
         // res.send('createProduct')
     }
